6da6bca77ef64a7e0b6f6922c5b4d43e6f7bead1
MatiasMingo <mamingo@uc.cl>
2020-06-18 01:22:54 -0400

    Heroku deployment 9

--- main.py
+++ main.py
@@ -1,42 +1,46 @@
 from market_data_analysis import market_data_compiler
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 sched = BlockingScheduler()
-"""
+
 def send_mail(stock_data):
-	nasdaq_list = stock_data["NASDAQ"]["{}".format(datetime.date(datetime.now()))]
-	nyse_list = stock_data["NYSE"]["{}".format(datetime.date(datetime.now()))]
-	content_list = []
-	content_list.append("NASDAQ")
+	nasdaq_list = stock_data["NASDAQ"]["{}".format(datetime.now())]
+	nyse_list = stock_data["NYSE"]["{}".format(datetime.now())]
+	content_html = '<h1><strong>Nasdaq</strong></h1>'
 	for stock in nasdaq_list[:10]:
 		stock_ticker = stock["ticker"]
 		opportunity = stock["opportunity_percentage"]
 		pe = stock["forwardPE"]
 		pb = stock["priceToBook"]
 		dividends = stock["trailingAnnualDividendRate"]
-		content_list.append("Stock: {}  Graham %: {}  PE: {}  PB: {}  Dividends: {}".format(stock_ticker, opportunity, pe, pb, dividends))
-	content_list.append("NYSE")
+		content_html.join("<br><p>Stock: {}  Graham %: {}%  PE: {}  PB: {}  Dividends: {}</p>".format(stock_ticker, opportunity, pe, pb, dividends))
+	content_html.join("<br><h1><strong>NYSE</strong></h1>")
 	for stock in nyse_list[:10]:
 		stock_ticker = stock["ticker"]
 		opportunity = stock["opportunity_percentage"]
 		pe = stock["forwardPE"]
 		pb = stock["priceToBook"]
 		dividends = stock["trailingAnnualDividendRate"]
-		content_list.append("Stock: {}  Graham %: {}  PE: {}  PB: {}  Dividends: {}".format(stock_ticker, opportunity, pe, pb, dividends))
-	complete_content = ""
-	for content in content_list:
-		complete_content.join("{} \n".format(content))
-    SERVER = ""
-    FROM = "mamingo@uc.cl"
-    TO = ["mamingo@uc.cl"]
-    SUBJECT = "Stock opportunities {}".format(datetime.datetime.now().date())
-    message = ""From: {}\r\nTo: {}\r\nSubject: {}\r\n
-    {}
-    ".format(FROM, ",".join(TO), SUBJECT, complete_content)
-    server = smtplib.SMTP(SERVER)
-    server.sendmail(FROM, TO, message)
-"""
+		content_html.join("<p>Stock: {}  Graham %: {}%  PE: {}  PB: {}  Dividends: {}</p>".format(stock_ticker, opportunity, pe, pb, dividends))
+	message = Mail(
+    from_email='mamingo@uc.cl',
+    to_emails='mamingo@uc.cl',
+    subject='Stock opportunities {}'.format(datetime.now),
+    html_content=content_html)
+	try:
+	    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+	    response = sg.send(message)
+	    print(response.status_code)
+	    print(response.body)
+	    print(response.headers)
+	except Exception as e:
+    	print(e.message)
+
+
 def get_data():
 	with open("market_data_analysis/stock_opportunities", 'r') as json_file:
 		json_dict = json.load(json_file)
@@ -45,8 +49,12 @@ def get_data():
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
     market_data_compiler.get_stock_data()
+    data_dict = get_data()
+	send_mail(data_dict)
 
 
 if __name__ == '__main__':
 	market_data_compiler.get_stock_data()
+	data_dict = get_data()
+	send_mail(data_dict)
 	sched.start()
