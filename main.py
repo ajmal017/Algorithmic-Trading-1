79946dee988f19e6f20a3b66394e376035508243
MatiasMingo <mamingo@uc.cl>
2020-06-17 22:10:58 -0400

    Heroku deployment 8

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from market_data_analysis import market_data_compiler
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+def scheduled_job():
+    pmarket_data_compiler.get_stock_data()
 
 
 if __name__ == '__main__':
-	market_data_compiler.get_stock_data()
+	sched.start()
